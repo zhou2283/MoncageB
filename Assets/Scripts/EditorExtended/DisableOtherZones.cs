7130d1650dce9b674df2d077d1c7738f2ad2cca9
DESKTOP-VES6UHG\dongz <dz1221@nyu.edu>
2018-10-24 23:54:43 -0400

    pre test

---
+++ Assets/Scripts/EditorExtended/DisableOtherZones.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DisableOtherZones : MonoBehaviour
+{
+    private static List<GameObject> zoneList = new List<GameObject>();
+
+    [MenuItem("Tools/DisableOtherZones %h")]
+    public static void change()
+    {
+        zoneList.Clear();
+        for (int i = 0; i < 5; i++)
+        {
+            zoneList.Add(GameObject.Find("ZoneGroup").transform.Find("Zone" + i.ToString()).gameObject);
+        }
+
+        foreach (GameObject child in zoneList)
+        {
+            child.SetActive(false);
+        }
+        GameObject[] currentZone = Selection.gameObjects;
+        foreach (GameObject child in currentZone)
+        {
+            child.SetActive(true);
+        }
+    }
+}
